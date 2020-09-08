7e1971e346a394da17c7b05178d2de77b24f54d2
Rian <godamri@local>
2020-09-08 17:05:32 +0700

    cors

--- src/Middleware/Cors.php
+++ src/Middleware/Cors.php
@@ -9,17 +9,20 @@ class Cors
 {
     public function handle($request, Closure $next)
     {
+        $allHeaders = app()->request->headers->all();
+        $allHeaders = array_merge([
+            'Content-Type',
+            'Authorization',
+            'X-Requested-With',
+            'X-User-Lang',
+        ], $allHeaders);
+
         $headers = [
             'Access-Control-Allow-Origin'      => implode(', ', config('cors.allowed_origins')),
             'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PATCH, PUT, DELETE',
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Max-Age'           => '86400',
-            'Access-Control-Allow-Headers'     => implode(', ', array_merge( config('cors.allowed_headers'), [
-                'Content-Type',
-                'Authorization',
-                'X-Requested-With',
-                'X-User-Lang',
-            ] ))
+            'Access-Control-Allow-Headers'     => implode(', ', array_merge( config('cors.allowed_headers'), $allHeaders ))
         ];
 
         if ($request->isMethod('OPTIONS'))
