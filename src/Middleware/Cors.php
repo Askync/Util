d2360a74c8be72e6fd1d6bfb565814c9fe2c4de3
Rian <godamri@local>
2020-09-09 14:38:19 +0700

    cors

--- src/Middleware/Cors.php
+++ src/Middleware/Cors.php
@@ -2,30 +2,25 @@
 namespace Askync\Utils\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-
 class Cors
 {
     public function handle($request, Closure $next)
     {
 
 
-        $allHeaders = $request->headers->all();
-        $allHeaders = array_merge([
+        $allowHeaders = [
             'Content-Type',
             'Authorization',
             'X-Requested-With',
             'X-User-Lang',
-        ], $allHeaders);
+        ];
 
         $headers = [
             'Access-Control-Allow-Origin'      => implode(', ', config('cors.allowed_origins')),
             'Access-Control-Allow-Methods'     => 'HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS',
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Max-Age'           => '86400',
-//            'Access-Control-Allow-Headers'     => (1== config('cors.allow_all_header')) ? '*' : implode(', ', array_merge( config('cors.allowed_headers'), $allHeaders ))
-            'Access-Control-Allow-Headers'     => $request->header('Access-Control-Request-Headers'),
+            'Access-Control-Allow-Headers'     => implode(', ', array_merge( config('cors.allowed_headers'), $allowHeaders ))
         ];
 
 
